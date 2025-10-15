1501cddb22d12db56d090427258f87b4f6bb66d0
Арсений Пьянков <pyankovasenya@gmail.com>
2025-10-15 22:32:14 +0700

    3

--- src/exercise3.c
+++ src/exercise3.c
@@ -1,6 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main(int argc,char** argv){
+    int k, i;
+    float a[10];
+    float b[10];
+    for(i = 0; i < 10; i++){
+        scanf("%f", &a[i]);
+    }
+    scanf("%d", &k);
+    k = k%10;
+    if(k<0)
+        k=10+k;
+    for(i = 0; i < 10; i++){
+        b[(i+k)%10] = a[i];
+    }
+    for(i = 0; i < 10; i++){
+        printf("%.2lf", b[i]);
+    }
+    printf("\n");
     return 0;
 }
