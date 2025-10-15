b6ab1c6cfafbefd63d4b079a5a8e51098a2e9eca
Арсений Пьянков <pyankovasenya@gmail.com>
2025-10-15 22:28:50 +0700

    1 gotovo

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-
-int main(int argc, char** argv) {
-    printf("Hello World!");
+#include <stdlib.h>
+int main(int argc, char** argv){
+    float array[10];
+    float sum = 0.0;
+    float x;    
+    for(int i = 0; i < 10; i++){
+        scanf("%f", &array[i]);
+        sum += array[i];
+    }
+    x = sum/10.0;
+    printf("%.3lf\n", x);
     return 0;
 }
