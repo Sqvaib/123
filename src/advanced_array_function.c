116e1b7ac6ddc8eeca799cf892d856bdace30a71
Арсений Пьянков <pyankovasenya@gmail.com>
2025-10-23 20:37:10 +0700

    1 Zadanie

--- src/advanced_array_function.c
+++ src/advanced_array_function.c
@@ -1,7 +1,22 @@
 #include "../include/advanced_array_function.h"
 
 int max_subarray_sum(int* nums, int size) {
-    return 0;
+    if (size <= 0){
+        return 0;
+    }
+    int maxs = nums[0];
+    int sum = nums[0];
+    for (int i = 1; i < size; i++){
+        if(sum < 0){
+            sum = nums[i];
+        } else{
+            sum += nums[i];
+        }
+        if(sum > maxs){
+            maxs = sum;
+        }
+    }
+    return maxs;
 }
 
 int length_of_lis(int* nums, int numsSize) {
